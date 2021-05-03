22aa28f1499bd9cbaa99cc92a56e163451af8509
Quadrate Tech Solutions <info@quadrate.lk>
2021-05-03 15:00:11 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string pal = Console.ReadLine();
+
+            for (int i = 0; i < pal.Length / 2; i++)
+            {
+                if (pal[i].ToString() != pal[pal.Length - (i+1)].ToString())
+                {
+                    Console.WriteLine("Not a palindrome");
+                    break;
+                }
+
+                if (i == (pal.Length / 2) - 1)
+                {
+                    Console.WriteLine("palindrome");
+                }                
+            }
+        }
+    }
+}
