3cbc2c591a7b4bda197ca6ba20e6561ec42aa6d9
Quadrate Tech Solutions <info@quadrate.lk>
2021-05-28 21:41:11 +0530

    if (i == (pal.Length / 2) - 1)                 {                     Console.WriteLine("palindrome");                 }

--- Program.cs
+++ Program.cs
@@ -4,14 +4,12 @@ namespace palindrome
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            
-            string pal = Console.ReadLine();
-
-            for (int i = 0; i < pal.Length / 2; i++)
+            var pal = Console.ReadLine();
+            for (var i = 0; i < pal.Length / 2; i++)
             {
-                if (pal[i].ToString() != pal[pal.Length - (i+1)].ToString())
+                if (pal[i].ToString() != pal[^(i+1)].ToString())
                 {
                     Console.WriteLine("Not a palindrome");
                     break;
@@ -22,6 +20,7 @@ namespace palindrome
                     Console.WriteLine("palindrome");
                 }                
             }
+            Console.ReadLine();
         }
     }
 }
